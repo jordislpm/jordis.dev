153ed483ad0d8bc54ef65d07b1adc11a3c23b483
jordislpm <jordislpm@gmail.com>
2022-11-13 23:41:24 -0400

    primer version funcional del maquetado

---
+++ proyectos js/funtions.js
@@ -0,0 +1,35 @@
+const carrusel =document.querySelector('.carrusel');
+const carruselItem =document.querySelector('.carrusel-item');
+const carruselItems =document.querySelector('.carrusel-items');
+
+let maxScrollLeft = carruselItems.scrollWidth - carruselItems.clientWidth;
+intervalo = null
+let step = 1;
+
+const start = ()=> {
+    intervalo = setInterval(function(){
+        carruselItems.scrollLeft = carruselItems.scrollLeft + step;
+        if (carruselItems.scrollLeft === maxScrollLeft){
+            step= step * -1;
+
+        } else if(carruselItems.scrollLeft === 0){
+            step = step * -1;
+        }
+    },10);
+
+};
+
+const stop = () => {
+    clearInterval(intervalo);
+  };
+  
+  
+  carruselItems.addEventListener("mouseover", () => {
+    stop();
+  });
+  
+  carruselItems.addEventListener("mouseout", () => {
+    start();
+  });
+  
+  start();
